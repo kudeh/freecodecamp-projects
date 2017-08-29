8e9d5571de2855a1587162ee400a9617830243dd
Kene Udeh <kene.udeh@yahoo.com>
2017-08-28 21:25:14 -0300

    Added Twitter Share Feature, for sharing quotes

--- Intermediate-Front-End-Development-Projects/Build a Random Quote Machine/js/script.js
+++ Intermediate-Front-End-Development-Projects/Build a Random Quote Machine/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
   $("#new-quote").click(function(){
-
+   
+   //Retrieve random quote from API.
    $.ajax({
     url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", 
     type: 'GET',
@@ -19,6 +20,16 @@ $(document).ready(function(){
       $(".author").append("<p>- "+author+"</p>");
       $(".quote").append(quote);
       
+      //strip out html tags from the string quote.
+      var div = document.createElement("div");
+      div.innerHTML = quote;      
+      quote = div.textContent || div.innerText || "";
+      
+      console.log(quote + " " + author);
+      
+      updateTwitterValues(quote, author);
+
+      
     },
     error: function(request,error)
     {
@@ -29,3 +40,11 @@ $(document).ready(function(){
  });
   
 });
+
+//update twitter share link.
+function updateTwitterValues(quote, author) {
+// clear out the <a> tag that's currently there...probably don't really need this since you're replacing whatever is in there already.
+  $("#share").html('&nbsp;'); 
+  $("#share").html('<a href="https://twitter.com/intent/tweet?text='+encodeURIComponent(quote)+'20%2D%20'+encodeURIComponent(author)+'" class="twitter-share-button" data-size="large">Tweet</a>');
+twttr.widgets.load();
+}
