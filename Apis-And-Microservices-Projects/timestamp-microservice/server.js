4780172398bf21952ed0995509a83ec7b6ee9994
kudeh <kene.udeh@yahoo.com>
2019-01-17 23:35:33 -0500

    Added routes for server

--- Apis-And-Microservices-Projects/timestamp-microservice/server.js
+++ Apis-And-Microservices-Projects/timestamp-microservice/server.js
@@ -6,4 +6,14 @@ app.get('/', function(req, res){
     res.sendFile(__dirname+'/views/index.html')
 })
 
+app.get('/:date_string', function(req, res){
+
+    var p = req.params.date_string;
+    var result = {}
+
+    res.send(result);
+
+    console.log(p);
+})
+
 app.listen(process.env.PORT, () => console.log('App Listening On Port ${process.env.PORT}..'))
