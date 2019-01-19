61273cb983b62a27a4d995694cf41cb2d33f4477
kudeh <kene.udeh@yahoo.com>
2019-01-19 16:31:47 -0500

    added cors package, and changed listener to log port number

--- Apis-And-Microservices-Projects/timestamp-microservice/server.js
+++ Apis-And-Microservices-Projects/timestamp-microservice/server.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 var express = require('express')
 var app = express()
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/views/index.html')
 })
@@ -32,4 +40,7 @@ app.get('/timestamp/:date_string', function(req, res){
     res.send(result);
 })
 
-app.listen(process.env.PORT, () => console.log('App Listening On Port ${process.env.PORT}..'))
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port + "..");
+  });
