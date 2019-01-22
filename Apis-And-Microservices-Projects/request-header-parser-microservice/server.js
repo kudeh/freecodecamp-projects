66321a6e18f79c41dd1ec8baf265c2b756462dd9
kudeh <kene.udeh@yahoo.com>
2019-01-21 21:10:22 -0500

    Added request handler for whoami endpoint, sent  request header info

---
+++ Apis-And-Microservices-Projects/request-header-parser-microservice/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/views/index.html')
+})
+
+app.get('/api/whoami', function(req, res){
+
+    var result_json = {};
+    result_json["ipaddress"] = req.ip;
+    result_json["language"] = req.get('User-Agent');
+    result_json["software"] = req.get('Accept-Language');
+
+    res.send(result_json);
+})
+
+var listener = app.listen(process.env.PORT, function(){
+    console.log('App is listening on ' + listener.address().port + '...')
+})
