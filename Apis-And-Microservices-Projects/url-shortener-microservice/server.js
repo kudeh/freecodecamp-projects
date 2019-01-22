29044045d6856e575ef2e55af7f81614be8c3621
kudeh <kene.udeh@yahoo.com>
2019-01-21 23:54:11 -0500

    imported static, bodyParser & cors middlewear

--- Apis-And-Microservices-Projects/url-shortener-microservice/server.js
+++ Apis-And-Microservices-Projects/url-shortener-microservice/server.js
@@ -1,8 +1,26 @@
 require('dotenv').config();
+var cors = require('cors');
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
+app.use(cors({optionsSuccessStatus: 200}));
+app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+"/views/index.html");
+})
+
+app.post('/api/shorturl/new', function(req, res){
+    res.send('shorten');
+})
 
 var listener = app.listen(process.env.PORT, function() {
     console.log('App is listening on port '+listener.address().port+'..');
