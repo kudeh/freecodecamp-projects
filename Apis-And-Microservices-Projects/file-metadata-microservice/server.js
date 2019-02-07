eae0bf7aa3a06b6852271e71217b556c8ea7a1cc
kudeh <kene.udeh@yahoo.com>
2019-02-06 19:21:58 -0500

    Installed Multer then added handler for getting uploaded file metadata

--- Apis-And-Microservices-Projects/file-metadata-microservice/server.js
+++ Apis-And-Microservices-Projects/file-metadata-microservice/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 var cors = require('cors');
 
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
+
 var express = require('express');
 var app = express();
 
@@ -9,11 +13,17 @@ app.use('/', express.static('public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html');
-})
+});
 
-app.post('/api/fileanalyse', function(req, res){
-    console.log('file upload');
-})
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+
+    if(req.file){
+        res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+    }else {
+        res.send({error: "no file uploaded"});
+    }
+    
+});
 
 var listener = app.listen(process.env.PORT, function(){
     console.log('App is listening on ' + listener.address().port + '..');
