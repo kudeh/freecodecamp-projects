0b34650ade6d9fff5fbb2e7a80370ba7933fab68
kudeh <kene.udeh@yahoo.com>
2019-06-10 20:03:36 -0400

    Added controller logic for conversions

--- Information-Security-And-Quality Assurance-Projects/metric-imperial-converter/controllers/convertHandler.js
+++ Information-Security-And-Quality Assurance-Projects/metric-imperial-converter/controllers/convertHandler.js
@@ -9,40 +9,108 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    var result;
+    
+    var result = null;
+    var splitIndex = input.search(/[A-Za-z]+/);
+    var num = input;
+
+    if(!(splitIndex === -1)){
+      num = input.slice(0, splitIndex);
+    }
+    
+    if(splitIndex === 0){
+       return 1;
+    }
+    
+    if(num.includes("/")){
+      var numArr = num.split("/");
+      if(numArr.length === 2){
+        numer = parseFloat(numArr[0]);
+        denom = parseFloat(numArr[1]);
+
+        if(!isNaN(numer) && !isNaN(denom)){
+          return numer/denom;
+        }
+      }
+    }
+    
+    if(!isNaN(num)){
+      if(parseFloat(num) >= 0){
+        result = parseFloat(num);
+
+      }
+    }
     
     return result;
   };
   
   this.getUnit = function(input) {
-    var result;
+    var result = null;
+    var possibleUnits = ['gal', 'lbs', 'mi', 'kg', 'l', 'km'];
+    var splitIndex = input.search(/[A-Za-z]+/);
+    var unit = input.slice(splitIndex).toLowerCase();
+    
+    if(possibleUnits.includes(unit)){
+      result = unit;
+    }
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
+    const mapping = {
+      lbs: 'kg',
+      gal: 'L',
+      mi: 'km',
+      kg: 'lbs',
+      L: 'gal',
+      km: 'mi'
+    }
+    
+    var result = mapping[initUnit];
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
+    
+    const mapping = {
+      lbs: 'pounds',
+      gal: 'gallons',
+      mi: 'miles',
+      kg: 'kilograms',
+      L: 'liters',
+      km: 'kilometers'
+    }
+    
+    var result = mapping[unit];
     
     return result;
   };
   
   this.convert = function(initNum, initUnit) {
+    
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
+    
+    const mapping = {
+      lbs: lbsToKg,
+      gal: galToL,
+      mi: miToKm,
+      kg: 1/lbsToKg,
+      L: 1/galToL,
+      km: 1/miToKm
+    }
+    
+    var result = initNum * mapping[initUnit];
     
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
+    var result= initNum + " " + this.spellOutUnit(initUnit) + 
+                " converts to " + returnNum + " " + this.spellOutUnit(returnUnit);
     
     return result;
   };
--- Information-Security-And-Quality Assurance-Projects/metric-imperial-converter/routes/api.js
+++ Information-Security-And-Quality Assurance-Projects/metric-imperial-converter/routes/api.js
@@ -18,13 +18,22 @@ module.exports = function (app) {
   app.route('/api/convert')
     .get(function (req, res){
       var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
+      var initNum = convertHandler.getNum(input).toFixed(5);
       var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
+
+      var returnNum = convertHandler.convert(initNum, initUnit).toFixed(5);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+      if(!(initNum) && !(initUnit)){
+        toString = "invalid number and unit";
+      }else if(!initNum){
+        toString = "invalid number";
+      }else if(!initUnit){
+        toString = "invalid unit";
+      }
       
-      //res.json
+      res.json({initNum: initNum, initUnit: initUnit, returnNum: returnNum, returnUnit: returnUnit, string: toString});
     });
     
 };
